2e6d0ff8132801d34ae22105226ae2eff4bfe547
ncortes12 <ncortes1415@gmail.com>
2018-08-08 17:49:27 -0700

    working site

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -5,40 +5,46 @@ var db = require("../models");
 
 
 //app.get will use the selectAll functions set up on orm.js and burgers.js to access the burgers_db and display all the burgers in the burger table
-module.exports = function (app){
-    console.log("loading")
-app.get("/", function (req, res) {
-    console.log("working")
-    db.burger.findAll().then(function(result){
-        console.log(result)
-        var hbsObject = {
-            burger:result
-        }
-        
-        res.render("index", hbsObject)
+module.exports = function (app) {
+
+    app.get("/", function (req, res) {
+        console.log("working")
+
+        db.burger.findAll().then(function (result) {
+            // console.log(result)
+            var hbsObject = {
+                burger: result
+            }
+
+            res.render("index", hbsObject)
+        })
     })
-})
 
-// // router.put will use the update functions set up in orm.js and burger.js and will update the devoured state of the burger from false to true
-// router.put("/api/burgers/:id", function (req, res) {
-//     var condition = req.params.id;
-//     console.log("condition", condition);
-//     console.log(req.body);
-//     burger.update(condition, function (result) {
-//         if (result.changedRows == 0) {
-//             return res.status(404).end();
-//         } else {
-//             res.status(200).end();
-//         }
-//     })
-// })
+    // // router.put will use the update functions set up in orm.js and burger.js and will update the devoured state of the burger from false to true
+    app.put("/api/burgers/:id", function (req, res) {
+        var condition = req.params.id;
+        console.log("condition", condition);
+        console.log(req.body);
+        // burger.update(condition, function (result) {
+        //     if (result.changedRows == 0) {
+        //         return res.status(404).end();
+        //     } else {
+        //         res.status(200).end();
+        //     }
+        // })
+        db.burger.update({ devoured: req.body.devoured },
+            {
+                where: { id: condition }
+            }).then(function (result) {
+                res.json(result)
+            })
+    })
 
-// //router.post will use the create functions set up in orm.js and burger.js and will add a burger to the burger table
-// router.post("/api/burgers", function (req, res) {
-//     console.log(req.body.name);
-//     burger.create([req.body.name], function (result) {
-//         res.json({ id: result.insertId })
-//     })
-// })
+    //app.post will use the create functions set up in orm.js and burger.js and will add a burger to the burger table
+    app.post("/api/burgers", function (req, res) {
+       db.burger.create({ burger_name: req.body.name }).then(function (result) {
+            res.json(result)
+        })
+    })
 }
 
--- models/burgers.js
+++ models/burgers.js
@@ -15,8 +15,12 @@ module.exports = function (sequelize, DataTypes) {
             type: DataTypes.BOOLEAN,
             allowNull: false,
             defaultValue: false
-        }
+        },
+        
+       
 
     });
+
     return burger;
+    
 } 
